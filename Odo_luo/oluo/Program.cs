96b09a5dbe9ac68461229010efaa0099213e8de4
Unknown <luo14938@gmail.com>
2022-06-30 22:21:28 +0200

    Program.cs Update

--- Odo_luo/oluo/Program.cs
+++ Odo_luo/oluo/Program.cs
@@ -3,11 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddMvc();
 
-builder.Services.AddControllersWithViews()
-    .AddRazorOptions(options => {
-    options.ViewLocationFormats.Add("/{1}/{0}.cshtml");
-    options.ViewLocationFormats.Add("/Shared/{0}.cshtml");
-});
+builder.Services.AddControllersWithViews();
+//    .AddRazorOptions(options => {
+//    options.ViewLocationFormats.Add("/{1}/{0}.cshtml");
+//    options.ViewLocationFormats.Add("/Shared/{0}.cshtml");
+//});
 builder.Services.AddRazorPages();
 
 
